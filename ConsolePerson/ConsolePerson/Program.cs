4b2abd87f11eb10dacb59facee9e72e7ed9f4daf
Mark Ohler <mark.ohler76@gmail.com>
2026-09-17 12:36:18 +0200

    Add files via upload

---
+++ ConsolePerson/ConsolePerson/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ConsolePerson;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Konsolen Programm";
+        Console.WriteLine("ConsolePerson");
+        // Eine neue Instanz/Objekt der Klasse Person anlegen und mittels Konstruktor Werte setzen
+        Person person1 = new Person("Harald","Müller",new DateTime(1990,8,21));
+        Person person2 = new Person("Heidi", "Klosterfrau", new DateTime(1762, 7, 13));
+        Console.WriteLine(person1.ToString());
+        Console.WriteLine(person2.ToString());
+        Console.ReadLine();
+    }
+}
+class Person
+{
+    // Attribute
+    private string vorname;
+    private string name;
+    public DateTime geburtstag;
+
+    // Konstruktor
+    public Person() { }  //Standartkonstruktor
+    public Person(string vorname, string name, DateTime geburtstag)
+    {
+        this.vorname = vorname;
+        this.name = name;
+        geburtstag = geburtstag;
+    }
+    public int Alter()
+    {
+        DateTime heute = DateTime.Now;
+        int alter = heute.Year - geburtstag.Year;
+        if ((heute.Month<geburtstag.Month) || (heute.Month == geburtstag.Month && heute.Day < geburtstag.Day))
+        {
+            alter -= 1;
+        }
+
+        return alter;
+    }
+
+    public override string ToString()
+    {
+        string str = "";
+        str += "\nVorname: " +vorname;
+        str += "\nNachname: " + name;
+        str += "\nGeburtstag: " + geburtstag.ToShortDateString();
+        str += "\nAlter: " + Alter();
+        return str;
+    }
+}
