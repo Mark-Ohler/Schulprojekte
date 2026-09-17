8b3d89edd5e1cc321005c5f22b056684d7de9c11
Mark Ohler <mark.ohler76@gmail.com>
2026-09-17 12:23:13 +0200

    Add files via upload

---
+++ BinärUmrechnen/BinärUmrechnen/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace BinärUmrechnen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Binärumrechner");
+                int eingabe = Eingabe();
+                string verarbeitung = Verarbeitung(eingabe);
+                Ausgabe(verarbeitung);
+                Console.WriteLine("Zum beenden q/Q drücken, für weiter bitte Enter drücken");
+                string loop = Console.ReadLine().ToLower();
+                if (loop == "q")
+                {
+                    break;
+                }
+            }
+        }
+        static int Eingabe()
+        {
+            Console.WriteLine("Bitte geben Sie Ihre Decimal Zahl ein ");
+            int eingabe = Convert.ToInt32(Console.ReadLine());
+            return eingabe;
+        }
+
+        static string Verarbeitung(int eingabe)
+        {
+            if (eingabe == 0)
+            {
+                return "0";
+            }
+            List<int> reste = new List<int>();
+            while (eingabe > 0)
+            {
+                int rest = eingabe % 2;
+                reste.Add(rest);
+                eingabe /= 2;
+            }
+            reste.Reverse();
+            string ausgabe = string.Join(" ", reste);
+            return ausgabe;
+        }
+        static void Ausgabe(string ausgabe)
+        {
+            Console.WriteLine("Ihre Binärzahl lautet: " + ausgabe);
+        }
+    }
+}
