c932e8ed227eef9c5570573a118ef65e17d857de
Mark Ohler <mark.ohler76@gmail.com>
2026-09-17 12:38:50 +0200

    Add files via upload

---
+++ ConsoleShape/ConsoleShape/Interfaces/AbstractShape.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleShape.Interfaces;
+
+// Abstrakte Klasse kann Implementierung enthalten
+// aber kann nicht instanziiert werden
+internal abstract class AbstractShape : IShape
+{
+    // gemeinsames Attribut
+    public string? Bezeichnung { get; set; }
+    // Konstruktor
+    protected AbstractShape(string? bezeichnung)
+    {
+        Bezeichnung = bezeichnung;
+    }
+    //Methoden
+    public abstract double BerechneVolumen();
+    public abstract double BerechneFläche();
+    // Bezeichnung ausgeben
+    public virtual string GetInfos()
+    {
+
+        return $"Bezeichnung: {Bezeichnung}";
+    }
+}
---
+++ ConsoleShape/ConsoleShape/Interfaces/IShape.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleShape.Interfaces;
+
+// Interface enthält keine Implementierung
+// und kann nicht instanziiert werden
+
+public interface IShape
+{
+    // öffentliche Methodenköpfe - lose Kopplung
+    public double BerechneVolumen();
+    public double BerechneFläche();
+    public string GetInfos();
+}
