fd3277694b3be0b42f77b0435991e7ca45fc0b44
Mark Ohler <mark.ohler76@gmail.com>
2026-09-17 12:21:53 +0200

    Add files via upload

---
+++ Addition/Addition/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition\n");
+
+            Console.Write("Bitte geben Sie die erste Zahl ein: ");
+            string zahl1 = Console.ReadLine();
+            double zahldouble = Convert.ToDouble(zahl1);
+
+            Console.Write("Bitte geben Sie die zweite Zahl ein: ");
+            double zahl2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Das Ergebnis Ihrer Addition ist: ");
+            Console.WriteLine(zahldouble + zahl2);
+            Console.ReadLine();
+
+        }
+    }
+}
